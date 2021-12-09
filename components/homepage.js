356d525144035b8a213f67c2309db985234d6614
Eva Demkova <eva.demkova@firma.seznam.cz>
2021-12-09 16:44:56 +0100

    fix layout

--- components/homepage.js
+++ components/homepage.js
@@ -11,10 +11,12 @@ const Homepage = () => {
 
 	return (
 		<div className={styles.welcome} onClick={showMoreInfo}>
-			<div className={styles.title}>Evka & Vavro</div>
-			<div className={styles.svd}>save the date</div>
-			<div className={styles.date}>13.8.2022</div>
-			<div className={styles.moreInfo} ref={nodeRef}>♥ ... viac informácií onedlho ... ♥</div>
+			<div className={styles.welcomeContainer}>
+			    <div className={styles.title}>Evka & Vavro</div>
+    			<div className={styles.svd}>save the date</div>
+    			<div className={styles.date}>13.8.2022</div>
+    			<div className={styles.moreInfo} ref={nodeRef}>♥ ... viac informácií onedlho ... ♥</div>
+			</div>
 		</div>
 	);
 };
