e9daa59da776a05701c621171f59c73a4c08ea4d
Eva Demkova <eva.demkova@firma.seznam.cz>
2021-12-16 15:07:39 +0100

    homepage styles changes

--- components/homepage.js
+++ components/homepage.js
@@ -7,15 +7,16 @@ const Homepage = () => {
 
     const showMoreInfo = () => {
         nodeRef.current.style.opacity = 1
+		nodeRef.current.style.visibility = "visible"
     }
 
 	return (
-		<div className={styles.welcome} onClick={showMoreInfo}>
+		<div className={styles.welcome} >
 			<div className={styles.welcomeContainer}>
 			    <div className={styles.title}>Evka & Vavro</div>
     			<div className={styles.svd}>save the date</div>
-    			<div className={styles.date}>13.8.2022</div>
-    			<div className={styles.moreInfo} ref={nodeRef}>♥ ... viac informácií onedlho ... ♥</div>
+    			<div className={styles.date} onClick={showMoreInfo}>13.8.2022</div>
+    			<div className={styles.moreInfo} ref={nodeRef}>... viac informácií onedlho ... </div>
 			</div>
 		</div>
 	);
