770988aa194d6053422446bfe691bcdf6c3d5914
Eva Demkova <evaademkova@gmail.com>
2022-02-17 11:33:31 +0100

    api failed

--- pages/api/sheets.js
+++ pages/api/sheets.js
@@ -5,44 +5,46 @@ async function handler(req, res) {
   if (req.method === 'POST') {
     const { body } = req;
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
-        private_key: privateKey.replace(/\\n/g, '\n'),
-      },
-      scopes: [
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/spreadsheets',
-      ],
-    });
+    // const auth = new google.auth.GoogleAuth({
+    //   credentials: {
+    //     client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    //     client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
+    //     private_key: privateKey.replace(/\\n/g, '\n'),
+    //   },
+    //   scopes: [
+    //     'https://www.googleapis.com/auth/drive',
+    //     'https://www.googleapis.com/auth/drive.file',
+    //     'https://www.googleapis.com/auth/spreadsheets',
+    //   ],
+    // });
 
-    const sheets = google.sheets({
-      auth,
-      version: 'v4',
-    });
+    // const sheets = google.sheets({
+    //   auth,
+    //   version: 'v4',
+    // });
 
-    const response = await sheets.spreadsheets.values.append({
-      spreadsheetId: process.env.GOOGLE_SHEETS_ID,
-      range: 'falala!A1:H5',
-      valueInputOption: 'USER_ENTERED',
-      insertDataOption: 'INSERT_ROWS',
-      requestBody: {
-        range: 'falala!A1:H5',
-        majorDimension: 'ROWS',
-        values: [[1, 2, 3]],
-      },
-    });
+    // const response = await sheets.spreadsheets.values.append({
+    //   spreadsheetId: process.env.GOOGLE_SHEETS_ID,
+    //   range: 'falala!A1:H5',
+    //   valueInputOption: 'USER_ENTERED',
+    //   insertDataOption: 'INSERT_ROWS',
+    //   requestBody: {
+    //     range: 'falala!A1:H5',
+    //     majorDimension: 'ROWS',
+    //     values: [[1, 2, 3]],
+    //   },
+    // });
 
-    console.log(response);
+    console.log(body);
+    res.status(200).json({ POST: body });
 
-    res.status(201).json(response);
+    // res.status(201).json(response);
     // res.send()
   }
-  res.status(200).json({ message: 'Hey!' });
 
-  // if(req.method === 'GET')
+  if (req.method === 'GET') {
+    res.status(200).json({ GET: 'Hey!' });
+  }
 }
 
 export default handler;
